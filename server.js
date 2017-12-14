208e405d3e76f73ad0892e3c6ceaeaed761d7bd1
jilljurgens <jurgensjill@gmail.com>
2017-12-13 18:12:43 -0600

    hi

--- server.js
+++ server.js
@@ -48,6 +48,10 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
 // Get all characters
 app.get("/all", function(req, res) {
   res.json(characters);
